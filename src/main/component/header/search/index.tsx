47e2a99839346ac4e075bc7a1dca5217a52c3317
Md Zafar Alam <zafaralam2608@gmail.com>
2025-03-14 01:25:20 +0530

    search redirect

--- src/main/component/header/search/index.tsx
+++ src/main/component/header/search/index.tsx
@@ -1,11 +1,9 @@
-import {Autocomplete, TextField} from '@mui/material'
+import {Autocomplete, Box, Link, TextField, Typography} from '@mui/material'
 import React, {useEffect, useState} from 'react'
 import {data} from '../../../constant/data'
 import {type SearchMultiResults} from '../../../interfaces/search'
-import {useNavigate} from "react-router-dom";
 
 const Search: React.FC = () => {
-  // const navigate = useNavigate();
   const [results, setResults] = useState<SearchMultiResults[]>([])
 
   useEffect(() => {
@@ -14,20 +12,19 @@ const Search: React.FC = () => {
 
   return (
     <Autocomplete
-      // freeSolo
       forcePopupIcon={false}
       disableClearable
-      // isOptionEqualToValue={(option, value) => }
       sx={{width: 582}}
-      onChange={(event , value) => {
-        // navigate(`/title/${value.id}`)
-        console.log(`/title/${value.id}`)
-        // navigate(`/search/${value.id}`)
-      }}
       options={results}
       renderInput={(params) => <TextField {...params} />}
       getOptionLabel={(option) => option.original_title}
-      // renderOption={(props, option) => {}
+      renderOption={(props, option) =>
+        <Box>
+          <Link href={`/title/${option.id}`}>
+            <Typography>{option.original_title}</Typography>
+          </Link>
+        </Box>
+      }
     />
   )
 }
