da459143fc8dc4c4371d1fe1090c1476fd031241
Md Zafar Alam <zafaralam2608@gmail.com>
2025-04-09 00:55:52 +0530

    Header style

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,36 +1,63 @@
-import { Menu } from "@mui/icons-material";
-import { AppBar, Button, Container, Toolbar, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { AppBar, Box, Button, Toolbar } from "@mui/material";
 import Search from "components/header/search";
 import React from "react";
 
 const Header: React.FC = () => {
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Button href="../">MovieDB</Button>
-          <Button>
-            <Menu sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              sx={{
-                mr: 2,
-                display: { xs: "none", md: "flex" },
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
+    <AppBar
+      sx={{
+        padding: "4px 0",
+      }}
+    >
+      <Toolbar disableGutters sx={{ margin: "0 120px", padding: "0 12px" }}>
+        <Button
+          href="../"
+          sx={{
+            width: "64px",
+            height: "32px",
+            padding: 0,
+            margin: "0 8px 0 0",
+          }}
+        >
+          MovieDB
+        </Button>
+        <Button
+          sx={{
+            width: "96px",
+            height: "32px",
+            padding: 0,
+            margin: 0,
+            textTransform: "none",
+          }}
+        >
+          <Box
+            sx={{
+              width: "100%",
+              height: "32px",
+              padding: "0 16px",
+              display: "flex",
+              flexDirection: "row",
+            }}
+          >
+            <MenuIcon sx={{ width: "24px", height: "24px" }} />
+            <span
+              style={{
+                width: "36px",
+                height: "18px",
+                marginLeft: "0.25rem",
+                lineHeight: "1.15rem",
+                fontSize: "0.875rem",
+                letterSpacing: ".02em",
+                fontWeight: 500,
               }}
             >
               Menu
-            </Typography>
-          </Button>
-          <Search />
-        </Toolbar>
-      </Container>
+            </span>
+          </Box>
+        </Button>
+        <Search />
+      </Toolbar>
     </AppBar>
   );
 };
