828dd5467c30b63db3217617000cc706edc65db6
Md Zafar Alam <zafaralam2608@gmail.com>
2025-04-16 23:55:23 +0530

    Revert "Header style"
    This reverts commit da459143fc8dc4c4371d1fe1090c1476fd031241.

--- src/components/header/search/index.tsx
+++ src/components/header/search/index.tsx
@@ -1,30 +1,13 @@
-import SearchIcon from "@mui/icons-material/Search";
-import {
-  Autocomplete,
-  Box,
-  Divider,
-  FormControl,
-  InputAdornment,
-  Link,
-  MenuItem,
-  Select,
-  SelectChangeEvent,
-  TextField,
-} from "@mui/material";
+import { Autocomplete, Box, Link, TextField, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useSearchMovieQuery } from "services/search";
 
 const Search: React.FC = () => {
-  const [qType, setQType] = useState("0");
   const [query, setQuery] = useState<string>("");
   const [q, setQ] = useState<string>("");
   const { data: results = [], isLoading } = useSearchMovieQuery(q, {
     skip: q.length < 3,
   });
-
-  const handleChange = (event: SelectChangeEvent) => {
-    setQType(event.target.value);
-  };
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
@@ -34,77 +17,26 @@ const Search: React.FC = () => {
   }, [query]);
 
   return (
-    <Box
-      sx={{
-        margin: "0 0.5rem",
-        display: "flex",
-        flexDirection: "row",
-        width: "600px",
-        height: "36px",
+    <Autocomplete
+      forcePopupIcon={false}
+      disableClearable
+      clearOnEscape
+      sx={{ width: 582 }}
+      loading={isLoading}
+      options={results}
+      onInputChange={(e, newValue) => {
+        setQuery(newValue);
       }}
-    >
-      <FormControl
-        size="small"
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          height: "32px",
-        }}
-      >
-        <Select value={qType} onChange={handleChange}>
-          <MenuItem value={0}>All</MenuItem>
-          <MenuItem value={2}>Titles</MenuItem>
-          <MenuItem value={3}>TV Episodes</MenuItem>
-          <MenuItem value={4}>Celebs</MenuItem>
-          <MenuItem value={5}>Companies</MenuItem>
-          <MenuItem value={6}>Keywords</MenuItem>
-          <Divider />
-          <MenuItem value={7}>Advanced Search</MenuItem>
-        </Select>
-        <Autocomplete
-          sx={{
-            width: 582,
-            display: "flex",
-            minHeight: "2.25rem",
-            "& .MuiOutlinedInput-root": {
-              padding: "0px!important",
-            },
-          }}
-          forcePopupIcon={false}
-          disableClearable
-          clearOnEscape
-          loading={isLoading}
-          options={results}
-          onInputChange={(e, newValue) => {
-            setQuery(newValue);
-          }}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              size="small"
-              style={{ padding: 0 }}
-              slotProps={{
-                input: {
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <SearchIcon width="24px" height="24px" />
-                    </InputAdornment>
-                  ),
-                },
-              }}
-            />
-          )}
-          getOptionLabel={(option) => option.original_title}
-          renderOption={(props, option) => (
-            <Box>
-              <Link href={`/title/${option.id}`}>
-                <span>{option.original_title}</span>
-              </Link>
-            </Box>
-          )}
-        />
-      </FormControl>
-    </Box>
+      renderInput={(params) => <TextField {...params} />}
+      getOptionLabel={(option) => option.original_title}
+      renderOption={(props, option) => (
+        <Box>
+          <Link href={`/title/${option.id}`}>
+            <Typography>{option.original_title}</Typography>
+          </Link>
+        </Box>
+      )}
+    />
   );
 };
 
