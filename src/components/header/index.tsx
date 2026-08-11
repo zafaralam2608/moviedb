61d344d8ff563000baf5ae367f8fea2f56a62c14
Md Zafar Alam <zafaralam2608@gmail.com>
2026-08-12 00:54:00 +0530

    update menu icon

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,30 +1,16 @@
-import { AppBar, Button, Container, Toolbar, Typography } from "@mui/material";
+import {AppBar, Button, Container, IconButton, Toolbar} from "@mui/material";
 import Search from "components/header/search";
 import React from "react";
+import {Menu} from "@mui/icons-material";
 
 const Header: React.FC = () => {
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <Button>
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              sx={{
-                mr: 2,
-                display: { xs: "none", md: "flex" },
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              Menu
-            </Typography>
-          </Button>
+          <IconButton>
+            <Menu />
+          </IconButton>
           <Button href="#/" sx={{ backgroundColor: "#f5c518", fontWeight: 900, color: "#000", letterSpacing: 1, textTransform: "none", m: 1 }}>
             MovieDB
           </Button>
