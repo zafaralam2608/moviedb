6337c81805410f9f19f2b3af45852013da44b09b
Md Zafar Alam <zafaralam2608@gmail.com>
2026-08-12 13:31:48 +0530

    fix menu icon

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,4 @@
-import {AppBar, Button, Container, IconButton, Toolbar} from "@mui/material";
+import {AppBar, Button, Container, Toolbar} from "@mui/material";
 import Search from "components/header/search";
 import React from "react";
 import {Menu} from "@mui/icons-material";
@@ -8,11 +8,11 @@ const Header: React.FC = () => {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <IconButton>
-            <Menu />
-          </IconButton>
           <Button href="#/" sx={{ backgroundColor: "#f5c518", fontWeight: 900, color: "#000", letterSpacing: 1, textTransform: "none", m: 1 }}>
             MovieDB
+          </Button>
+          <Button startIcon={<Menu />} sx={{ textTransform: "none", color: "#fff", m: 1 }}>
+            Menu
           </Button>
           <Search />
         </Toolbar>
